f14416a78788116152248b10d5111b50d85715ca
liYang <Kevin.Li@tianyitechs.com>
2017-08-23 10:41:28 +0800

    fix readme and add contacts.sql

--- README.md
+++ README.md
@@ -1,6 +1,19 @@
 
 # contacts
-angular2+express 的集成项目， 有开发和测试环境
+angular2+express+mysql
+
+## mysql
+service/querySql 中设置连接mysql的 user和password
+并附上mysql文件
+
+## 开发环境
+`npm run start`
+前台项目: localhost:4200
+后台服务地址: localhost:4001
+
+## 生产环境
+`npm run build`
+localhost:4001
 
 
 
---
+++ contacts.sql
@@ -0,0 +1,4 @@
+INSERT INTO `contacts` (`name`, `phone`, `isCollected`, `email`, `address`, `selfIntroduction`, `id`, `sex`) VALUES ('董小茹1', '1381356486', 0, 'l546872118@163.com', '北京昌平', '我是一个程序猿，哈哈哈', 1, 'male');
+INSERT INTO `contacts` (`name`, `phone`, `isCollected`, `email`, `address`, `selfIntroduction`, `id`, `sex`) VALUES ('李洋', '3546813', 0, '812875741@qq.com', '新河县苏章村', '我也是一个程序员', 2, 'male');
+INSERT INTO `contacts` (`name`, `phone`, `isCollected`, `email`, `address`, `selfIntroduction`, `id`, `sex`) VALUES ('小茹茹', '7654321', 1, '4515@qq.com', '还闹到', '我是一个小木匠', 3, 'male');
+INSERT INTO `contacts` (`name`, `phone`, `isCollected`, `email`, `address`, `selfIntroduction`, `id`, `sex`) VALUES ('小黄', '234234', 1, '大师傅大师傅', '大厦法定身份a', '你4不4傻', 4, 'male');
