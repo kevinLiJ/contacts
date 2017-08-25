cc84eeb38312286420eefc2cac9b5ca19cb28f77
liYang <Kevin.Li@tianyitechs.com>
2017-08-25 18:29:09 +0800

    add search contact

--- src/app/contactList/contactlist.component.ts
+++ src/app/contactList/contactlist.component.ts
@@ -1,19 +1,33 @@
 import { Component,OnInit } from '@angular/core';
 import { ActivatedRoute} from '@angular/router';
+import { FormGroup, FormControl} from '@angular/forms';
 import { ContactService } from '../shared/service/contact.service';
 
 @Component({
   selector: 'contact-list',
-  templateUrl: './contactlist.component.html'
+  templateUrl: './contactlist.component.html',
+  styleUrls:['./contactlist.component.css']
 })
 
 
 export class ContactListComponent {
-    ngOnInit(){
-        this.getContact()
-    }
-
+    
     constructor(private _contactService:ContactService){}
+    contacts:any[] = [];
+    ngOnInit(){
+        this.getContact();
+        this.searchContactsForm.controls.term.valueChanges
+        .debounceTime(500)
+        .distinctUntilChanged()
+        .subscribe(termVal => {
+            if(termVal === '') {this.getContact()}
+            this.contacts = this.contacts.filter( contacts => contacts.name.indexOf(termVal) > -1)
+        })
+    }
+    
+    searchContactsForm = new FormGroup({
+        term: new FormControl()
+    })
 
     getContact(){
         return this._contactService.getContacts()
@@ -24,7 +38,4 @@ export class ContactListComponent {
             error => console.log(error.message)
         )
     }
-    
-    contacts:any[] = [];
-
 }
